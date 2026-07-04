2dcdf14ca600b4a1933dc7d78c07c1c43b571a5c
trtrgtri <trtrgtri27@gmail.com>
2026-07-04 21:51:34 +0700

    Clear database password in configuration
    Reset the database password to an empty string.

--- config/database.php
+++ config/database.php
@@ -4,6 +4,6 @@ return [
     'host' => '127.0.0.1',
     'database' => 'equipment_rental_crm',
     'username' => 'root',
-    'password' => '0826411454_Tt',
+    'password' => '',
     'charset' => 'utf8mb4',
 ];
